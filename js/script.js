36d8eee3e56fbe73589d9752285750be5def4730
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-26 20:58:19 -0300

    lista de estudantes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Criar uma lista de **estudantes** e, 
+ * cada estudante dentro dessa lista, deverá conter os seguintes dados:
+
+- nome;
+- nota da primeira prova;
+- nota da segunda prova.
+Depois de criada a lista:
+- Crie uma **função** que irá calcular a média das notas de cada aluno;
+- Supondo que a média, para esse concurso é **7**, verifique **se** cada 
+aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.
+ */
+
+const students = [
+  {
+    name: 'Grande Nerd',
+    noteOne: 10,
+    noteTwo: 10,
+  },
+  {
+    name: 'Dorameira da Lua',
+    noteOne: 6,
+    noteTwo: 7,
+  },
+  {
+    name: 'Estudante neurotico',
+    noteOne: 8,
+    noteTwo: 7,
+  },  
+]
+
