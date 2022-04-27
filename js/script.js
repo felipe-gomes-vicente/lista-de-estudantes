8e13a3bbc2f4011203185f2b27ef8410a2be7be1
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-04-26 21:59:05 -0300

    ajuste na estrutura de repetição e media, criação da função de aprovados

--- js/script.js
+++ js/script.js
@@ -12,29 +12,44 @@ Depois de criada a lista:
 aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.
  */
 
+// Data structure with objects
 const students = [
   {
-    name: 'Grande Nerd',
+    name: "Grande Nerd",
     noteOne: 10,
     noteTwo: 10,
   },
   {
-    name: 'Dorameira na Lua',
+    name: "Dorameira na Lua",
     noteOne: 6,
     noteTwo: 7,
   },
   {
-    name: 'Estudante neurótico',
+    name: "Estudante neurótico",
     noteOne: 8,
     noteTwo: 7,
-  },  
-]
+  },
+];
 
+// Function Average
 function average(noteOne, noteTwo) {
-  result = (noteOne + noteTwo)/ 2 
-  return result
+  return (noteOne + noteTwo) / 2;
 }
 
-for ( let student of students ) {
-  alert(` A media do(a) Aluno(a) ${student.name} é: ${average(student.noteOne, student.noteTwo)}`)
+//Comparative operators 
+function accredited(student) {
+  result = average(student.noteOne, student.noteTwo);
+  if (result >= 7) {
+    return `    A media do(a) Aluno(a) ${student.name} é: ${result} 
+    Parabéns ${student.name}, você passou no Concurso!`;
+  } else {
+    return `    A media do(a) Aluno(a) ${student.name} é: ${result} 
+    Não Foi dessa vez ${student.name}, Tente Novamente!`;
+  }
+}
+
+// Repeat structure
+for (let student of students) {
+  let showResult = accredited(student);
+  alert(showResult);
 }
